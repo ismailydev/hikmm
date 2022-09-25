4ee6574a9e9cc4250bfaa17a78e81aec9ff19eff
Ismail <ismail.yimer.h@gmail.com>
2022-09-25 12:26:13 +0300

    Added TailwindCSS

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 const connectDB = require("./config/db");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 8000;
 const app = express();
 
-const PORT = process.env.PORT || 7000;
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 connectDB();
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
