a7a1576e4fc868edfc04862c89a6e34348bd7329
Ismail <ismail.yimer.h@gmail.com>
2022-09-26 09:28:03 +0300

    added passport

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const passport = require("passport");
 const path = require("path");
-const connectDB = require("./config/db");
+
+const connectDB = require("./config/database");
+
 require("dotenv").config();
+require("./config/passport")(passport);
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -21,6 +25,13 @@ connectDB();
 
 app.get("/", (req, res) => {
     res.render("index");
+});
+app.use("/auth", require("./routes/auth"));
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+app.get("/signin", (req, res) => {
+    res.render("signin");
 });
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
