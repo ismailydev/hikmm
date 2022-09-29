682b59eecd4fafaaa529878d9a82b16a4c5a9f19
Ismail <ismail.yimer.h@gmail.com>
2022-09-29 19:48:16 +0300

    fixed passport authentication

--- config/passport.js
+++ config/passport.js
@@ -12,24 +12,24 @@ module.exports = function (passport) {
             },
             async function (acessToken, refreshToken, profile, done) {
                 console.log(profile);
-                // const newUser = {
-                //     googleId: profile.id,
-                //     displayName: profile.displayName,
-                //     firstName: profile.name.givenName,
-                //     lastName: profile.name.familyName,
-                //     image: profile.photos[0].value,
-                // };
-                // try {
-                //     let user = await User.findOne({ googleId: profile.id });
-                //     if (user) {
-                //         done(null, user);
-                //     } else {
-                //         user = await User.create(newUser);
-                //         done(null, user);
-                //     }
-                // } catch (error) {
-                //     console.log(error);
-                // }
+                const newUser = {
+                    googleId: profile.id,
+                    displayName: profile.displayName,
+                    firstName: profile.name.givenName,
+                    lastName: profile.name.familyName,
+                    image: profile.photos[0].value,
+                };
+                try {
+                    let user = await User.findOne({ googleId: profile.id });
+                    if (user) {
+                        done(null, user);
+                    } else {
+                        user = await User.create(newUser);
+                        done(null, user);
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
             }
         )
     );
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const path = require("path");
 
 const connectDB = require("./config/database");
@@ -23,15 +25,28 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 connectDB();
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.use(
+    session({
+        secret: "hikmm",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.DB_STRING,
+        }),
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+    res.locals.user = req.user;
+    next();
 });
+
+app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
-app.get("/signup", (req, res) => {
-    res.render("signup");
-});
-app.get("/signin", (req, res) => {
-    res.render("signin");
-});
+app.use("/store", require("./routes/store"));
+app.use("*", (req, res) => res.render("404"));
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
