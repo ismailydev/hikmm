02ab3fb7edc5bc55f71290c7265111ff141df90b
Ismail <ismail.yimer.h@gmail.com>
2022-10-04 09:39:18 +0300

    local authentication feature

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require("mongoose");
+const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
 
 module.exports = function (passport) {
@@ -8,7 +8,8 @@ module.exports = function (passport) {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: "/auth/google/callback",
+                callbackURL:
+                    `${process.env.HOST || ""}` + "/auth/google/callback",
             },
             async function (acessToken, refreshToken, profile, done) {
                 console.log(profile);
@@ -30,6 +31,39 @@ module.exports = function (passport) {
                 } catch (error) {
                     console.log(error);
                 }
+            }
+        )
+    );
+    passport.use(
+        new LocalStrategy(
+            { usernameField: "email" },
+            (email, password, done) => {
+                User.findOne({ email: email.toLowerCase() }, (err, user) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!user) {
+                        return done(null, false, {
+                            msg: `Email ${email} not found.`,
+                        });
+                    }
+                    if (!user.password) {
+                        return done(null, false, {
+                            msg: "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
+                        });
+                    }
+                    user.comparePassword(password, (err, isMatch) => {
+                        if (err) {
+                            return done(err);
+                        }
+                        if (isMatch) {
+                            return done(null, user);
+                        }
+                        return done(null, false, {
+                            msg: "Invalid email or password.",
+                        });
+                    });
+                });
             }
         )
     );
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+
 const path = require("path");
 
 const connectDB = require("./config/database");
@@ -22,6 +24,17 @@ app.use(express.json());
 app.use(express.static("public"));
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === "object" && "_method" in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method;
+            delete req.body._method;
+            return method;
+        }
+    })
+);
 
 connectDB();
 
